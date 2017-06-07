29ac3ae80193c5bd05e28891ba4340ddab4904fa
abdullahalam <abdullah_alam_omi@outlook.com>
2017-06-07 06:18:04 +0000

    Add a custom before_action and current_user is enrolled in the course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,17 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_authorized_for_current_section
+    if current_user.enrolled_in?(current_lesson.section.course)
+      
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
