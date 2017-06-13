e003e78cb25e4c4b4230e74ad7140ddd337debf6
abdullahalam <abdullah_alam_omi@outlook.com>
2017-06-13 08:36:35 +0000

    Add cleaning up sections controller page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -20,7 +20,7 @@ class Instructor::SectionsController < ApplicationController
   private
 
   def require_authorized_for_current_section
-    if current_section.course.course.user != current_user
+    if current_section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
